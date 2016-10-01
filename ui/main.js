d552b7fcd55de8ad9ec91e7ead146e009bfe7145
chandanachowdary <venigalla.chandana@gmail.com>
2016-10-01 16:28:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,11 @@ var element= document.getElementById('main-text');
 element.innerHTML='new value';
 // to move an elemnt on click
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
     marginLefft=marginLeft+2;
     img.style.marginleft=marginLeft+'px';
 }
 img.onClick=function(){
-    var interval=setinterval(moveRight,10);
+    var interval=setInterval(moveRight,10);
 };
